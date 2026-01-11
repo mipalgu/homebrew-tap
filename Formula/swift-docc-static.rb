ed4cea2299b54ddef7a04de0949ec9e915ac3831
Rene Hexel <r.hexel@griffith.edu.au>
2026-01-11 12:04:07 +1000

    Update swift-docc-static to v0.1.3 with pre-built bottles

--- Formula/swift-docc-static.rb
+++ Formula/swift-docc-static.rb
@@ -2,10 +2,16 @@ class SwiftDoccStatic < Formula
   desc "Generate static HTML/CSS documentation for Swift packages"
   homepage "https://github.com/mipalgu/swift-docc-static"
   url "https://github.com/mipalgu/swift-docc-static/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "03928b89eb771db88f76215a883ca0aa1da7d4d98e2cc81606c3c6d52e55a7d9"
+  sha256 "11b1f73c91fa14187734c6bfb86bd69eb15d523de99d6a712b1a40d0e388570c"
   license "Apache-2.0"
   env :std
   head "https://github.com/mipalgu/swift-docc-static.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/mipalgu/swift-docc-static/releases/download/v0.1.3"
+    sha256 cellar: :any, arm64_sequoia:   "236a46bc58439556c861cd4f7c7018a2a33e5503df71a3568e107087e1a52069"
+    sha256 cellar: :any, x86_64_linux:    "d35ce3d26365c55e7b721f55282015a9b0a01f720312766f40539072a78925c8"
+  end
 
   on_macos do
     depends_on :xcode => ["26.0", :build]
